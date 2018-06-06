5bc4c41c1bdf6fcddd60758de4d0574a6fc8f06f
hidetoshi.kanno <taf71811@docomo.ne.jp>
2018-06-06 16:59:15 +0900

    2018/6/6 Kanno

--- SlideshowApp/ExpandPictureViewController.swift
+++ SlideshowApp/ExpandPictureViewController.swift
@@ -15,9 +15,7 @@ class ExpandPictureViewController: UIViewController {
     
     // その他変数
     var image_1: UIImage! = nil     //画像を格納する変数
-    var width_tmp: CGFloat = CGFloat(1.0)
-    var height_tmp: CGFloat = CGFloat(1.0)
-    
+
     
     
     
@@ -36,34 +34,13 @@ class ExpandPictureViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // 各パラメータの初期化
-        width_tmp = CGFloat(1.0)
-        height_tmp = CGFloat(1.0)
-    }
     
-
+    
+    
     // ピンチジェスチャー時の動作
     @IBAction func PinchAction_1(_ sender: UIPinchGestureRecognizer) {
-        print(sender.scale)
-        
-        // 横倍率設定
-        if sender.scale >= 1.3 {
-            width_tmp = UIImageView_1.frame.size.width * CGFloat(1.3)
-            height_tmp = UIImageView_1.frame.size.height * CGFloat(1.3)
-        }
-        else if sender.scale <= 0.7 {
-            width_tmp = UIImageView_1.frame.size.width * CGFloat(0.7)
-            height_tmp = UIImageView_1.frame.size.height * CGFloat(0.7)
-        }
-        else {
-            width_tmp = UIImageView_1.frame.size.height * sender.scale
-            height_tmp = UIImageView_1.frame.size.height * sender.scale
-        }
-        
-        UIImageView_1.frame.size = CGSize(width: width_tmp, height: height_tmp)
+        // 画像のスケールを変更する。
+        UIImageView_1.transform = CGAffineTransform(scaleX: sender.scale, y: sender.scale)
     }
     
 
